1e8048232edc06118fddb2918a1bc8dc0287f020
Andrey Lepikhov (auto) <andrey.lepikhov@devexpress.com>
2020-01-24 10:33:30 +0300

    auto sync for version 2019.2

---
+++ CS/Reporting_How-To-Add-WebDocumentViewer-To-AspNetCoreProj/SampleReport.cs
@@ -0,0 +1,13 @@
+using System;
+using DevExpress.XtraReports.UI;
+
+namespace sampleAspNetReporting
+{
+    public partial class SampleReport
+    {
+        public SampleReport()
+        {
+            InitializeComponent();
+        }
+    }
+}
